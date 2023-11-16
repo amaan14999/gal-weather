ae576f012c40b5ef65ce9e61724133b76e1c740b
Amaan <amaan14999@gmail.com>
2023-11-16 13:39:07 +0530

    fix: country

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -19,10 +19,10 @@ export type WeatherResponse = {
 export class WeatherService {
   constructor(private http: HttpClient) {}
 
-  forCountry(country: String): Observable<WeatherResponse> {
+  forCountry(country: string): Observable<WeatherResponse> {
     const params = new URLSearchParams();
     params.set('APPID', '794ee95e63c5a32aaf88cd813fa2e425');
-    params.set('q', 'Afghanistan');
+    params.set('q', country);
 
     return this.http.get<WeatherResponse>(
       `https://api.openweathermap.org/data/2.5/weather?${params.toString()}`
